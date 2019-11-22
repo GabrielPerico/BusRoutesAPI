6a4baebfeb3dd5b46ee84aab79cd314510a62491
Gabriel <gabriel.perico@aluno.sc.senac.br>
2019-11-21 23:07:04 -0200

    arrumei umas coisa

--- application/models/Usuario_model.php
+++ application/models/Usuario_model.php
@@ -14,7 +14,10 @@ class Usuario_model extends CI_Model{
     public function registerUsuario($data = array())
     {
         $this->db->insert('usuario',$data);
-        return $this->db->affected_rows();
+        $lastid = $this->db->insert_id();
+        $this->db->where('id', $lastid);
+        $query = $this->db->get('usuario');
+        return $query->row();
     }
 
     public function checkEmail($email)
