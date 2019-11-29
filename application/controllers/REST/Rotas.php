f6246bd3bf083afb716628ce80199ff96c4e2321
Gabriel <gabriel.perico@aluno.sc.senac.br>
2019-11-28 22:58:51 -0200

    num guento mais

---
+++ application/controllers/REST/Rotas.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Implementação da API rest usando a biblioteca do link abaixo
+ * Essa biblioteca possui quatro arquivos distintos:
+ * 1 - REST_Controller na pasta libraries, que altera o comportamento padrão das controllers padrões do CI
+ * 2 - REST_Controller_Definitions na pasta libraries, que tras algumas definições para o REST_Controller,
+ *     trabalha como um arquivo de padrões auxiliando o controller principal
+ * 3 - Format na pasta Libraries, que faz o parsing (conversão) dos diferentes tipos de dados (JSON, XML, CSV e etc)
+ * 4 - rest.php na pasta config, para as configurações desta biblioteca
+ * 
+ * @author      Aluno Gabriel Périco
+ * @link        https://github.com/chriskacerguis/codeigniter-restserver
+ */
+
+use Restserver\Libraries\REST_Controller;
+use Restserver\Libraries\REST_Controller_Definitions;
+
+
+require APPPATH . '/libraries/REST_Controller.php';
+require APPPATH . '/libraries/REST_Controller_Definitions.php';
+require APPPATH . '/libraries/Format.php';
+
+class Rotas extends Rest_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index_post()
+    {
+        $this->load->model('Rotas_model');
+        $rota = $this->post(0);
+
+        $rotaArray = array(
+            'lat_inicial' => $rota['lat_inicial'],
+            'lat_final' => $rota['lat_final'],
+            'lng_inicial' => $rota['lng_inicial'],
+            'lng_final' => $rota['lng_final'],
+            'id_onibus' => $this->get('id'),
+            'horario_ini' => $rota['horario_ini'],
+            'horario_fim' => $rota['horario_fim'],
+            'v_passagem' => $rota['v_passagem'],
+            'v_passagem_estudante' => $rota['v_passagem_estudante']
+        );
+        $idRota = $this->Rotas_model->insertRota($rotaArray);
+        $paradas = $this->post();
+        for ($i = 1; $i < sizeof($paradas); $i++) {
+            $data['lat']    = $paradas[$i]['lat'];
+            $data['lng']     = $paradas[$i]['lat'];
+            $data['horario'] = $paradas[$i]['horario'];
+            $data['id_rota'] = $idRota;
+            $this->Rotas_model->insertParadas($data);
+        }
+
+
+        /**/
+
+        //$this->set_response($this->post()[1], REST_Controller_Definitions::HTTP_OK);
+    }
+}
--- busroutes.sql
+++ busroutes.sql
@@ -25,9 +25,15 @@ CREATE TABLE IF NOT EXISTS `empresa` (
   `telefone` varchar(15) DEFAULT NULL,
   `token` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela busroutes.empresa: ~0 rows (aproximadamente)
+DELETE FROM `empresa`;
+/*!40000 ALTER TABLE `empresa` DISABLE KEYS */;
+INSERT INTO `empresa` (`id`, `nome`, `email`, `senha`, `telefone`, `token`) VALUES
+	(1, 'padrão', 'a@a', 'aosjdaw', '131231231', 'token_padrao');
+/*!40000 ALTER TABLE `empresa` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela busroutes.localizacao_onibus
 CREATE TABLE IF NOT EXISTS `localizacao_onibus` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -40,7 +46,11 @@ CREATE TABLE IF NOT EXISTS `localizacao_onibus` (
   CONSTRAINT `FK_Onibus_loc` FOREIGN KEY (`id_onibus`) REFERENCES `onibus` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela busroutes.localizacao_onibus: ~0 rows (aproximadamente)
+DELETE FROM `localizacao_onibus`;
+/*!40000 ALTER TABLE `localizacao_onibus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `localizacao_onibus` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela busroutes.noticias
 CREATE TABLE IF NOT EXISTS `noticias` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -51,9 +61,16 @@ CREATE TABLE IF NOT EXISTS `noticias` (
   PRIMARY KEY (`id`),
   KEY `FK_Usuario` (`id_usuario`),
   CONSTRAINT `FK_Usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela busroutes.noticias: ~5 rows (aproximadamente)
+DELETE FROM `noticias`;
+/*!40000 ALTER TABLE `noticias` DISABLE KEYS */;
+INSERT INTO `noticias` (`id`, `titulo`, `descricao`, `minidescricao`, `id_usuario`) VALUES
+	(1, 'Where does it come from?', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. \r\nLorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type \r\nand scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap\r\n into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem IpsumLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, \r\nwhen an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,\r\n remaining essentially unchanged. It was popularised in the 1960s with\r\n the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum', 'Lorem Ipsum is simply dummy text', 1),
+	(2, 'Noticia', 'uma descrição bem básica só pra testa', 'descriçãozinha', 1);
+/*!40000 ALTER TABLE `noticias` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela busroutes.onibus
 CREATE TABLE IF NOT EXISTS `onibus` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -67,9 +84,37 @@ CREATE TABLE IF NOT EXISTS `onibus` (
   PRIMARY KEY (`id`),
   KEY `FK_Empresa_onibus` (`id_empresa`),
   CONSTRAINT `FK_Empresa_onibus` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela busroutes.onibus: ~0 rows (aproximadamente)
+DELETE FROM `onibus`;
+/*!40000 ALTER TABLE `onibus` DISABLE KEYS */;
+INSERT INTO `onibus` (`id`, `placa`, `modelo`, `intermunicipal`, `img_onibus`, `id_empresa`, `data_registro`, `ativo`) VALUES
+	(1, '1231', '123', 0, NULL, 1, '2019-11-28 22:54:59', 1);
+/*!40000 ALTER TABLE `onibus` ENABLE KEYS */;
+
+-- Copiando estrutura para tabela busroutes.paradas
+CREATE TABLE IF NOT EXISTS `paradas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lat` float NOT NULL,
+  `lng` float NOT NULL,
+  `horario` time NOT NULL,
+  `id_rota` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_paradas_rotas` (`id_rota`),
+  CONSTRAINT `FK_paradas_rotas` FOREIGN KEY (`id_rota`) REFERENCES `rotas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela busroutes.paradas: ~0 rows (aproximadamente)
+DELETE FROM `paradas`;
+/*!40000 ALTER TABLE `paradas` DISABLE KEYS */;
+INSERT INTO `paradas` (`id`, `lat`, `lng`, `horario`, `id_rota`) VALUES
+	(2, 123.1, 123.1, '01:02:00', 6),
+	(3, 123.1, 123.1, '01:02:00', 6),
+	(4, 123.1, 123.1, '01:02:00', 6),
+	(5, 123.1, 123.1, '01:02:00', 6);
+/*!40000 ALTER TABLE `paradas` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela busroutes.promocao
 CREATE TABLE IF NOT EXISTS `promocao` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -81,7 +126,11 @@ CREATE TABLE IF NOT EXISTS `promocao` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela busroutes.promocao: ~0 rows (aproximadamente)
+DELETE FROM `promocao`;
+/*!40000 ALTER TABLE `promocao` DISABLE KEYS */;
+/*!40000 ALTER TABLE `promocao` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela busroutes.promocoes_empresa
 CREATE TABLE IF NOT EXISTS `promocoes_empresa` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -94,7 +143,11 @@ CREATE TABLE IF NOT EXISTS `promocoes_empresa` (
   CONSTRAINT `FK_Promocao` FOREIGN KEY (`id_promocao`) REFERENCES `promocao` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela busroutes.promocoes_empresa: ~0 rows (aproximadamente)
+DELETE FROM `promocoes_empresa`;
+/*!40000 ALTER TABLE `promocoes_empresa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `promocoes_empresa` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela busroutes.rotas
 CREATE TABLE IF NOT EXISTS `rotas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -110,21 +163,34 @@ CREATE TABLE IF NOT EXISTS `rotas` (
   PRIMARY KEY (`id`),
   KEY `FK_Onibus_rotas` (`id_onibus`),
   CONSTRAINT `FK_Onibus_rotas` FOREIGN KEY (`id_onibus`) REFERENCES `onibus` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela busroutes.rotas: ~0 rows (aproximadamente)
+DELETE FROM `rotas`;
+/*!40000 ALTER TABLE `rotas` DISABLE KEYS */;
+INSERT INTO `rotas` (`id`, `lat_inicial`, `lng_inicial`, `lat_final`, `lng_final`, `id_onibus`, `horario_ini`, `horario_fim`, `v_passagem`, `v_passagem_estudante`) VALUES
+	(6, 1, 2, 1, 2, 1, '01:01:00', '02:02:00', 3, 1.5);
+/*!40000 ALTER TABLE `rotas` ENABLE KEYS */;
+
 -- Copiando estrutura para tabela busroutes.usuario
 CREATE TABLE IF NOT EXISTS `usuario` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(60) NOT NULL,
   `email` varchar(80) NOT NULL,
-  `senha` varchar(30) NOT NULL,
+  `senha` varchar(45) NOT NULL,
   `admin` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela busroutes.usuario: ~1 rows (aproximadamente)
+DELETE FROM `usuario`;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` (`id`, `nome`, `email`, `senha`, `admin`) VALUES
+	(1, 'percio', 'a@a', 'd9f85236a3f7d17b2311ef98953ebf233700ac82', 0),
+	(10, 'a', 'a', '0c366ef9026c6f7c6b1f2f97b480649437a2f294', 0);
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
